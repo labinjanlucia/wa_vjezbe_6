9594730f9fd9a8e5d692cab1bd3120148559531e
labinjanlucia <134169024+labinjanlucia@users.noreply.github.com>
2025-01-13 21:06:29 +0100

    postavila osnovne komponente index.js data folder za podatke

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+const PORT = 3003;
+app.use(express.json());
+
+
+
+
+ 
+app.listen(PORT, (error) => {
+if (error) {
+console.error(`Greška prilikom pokretanja poslužitelja: ${error.message}`);
+} else {
+console.log(`Server je pokrenut na http://localhost:${PORT}`);
+}
+});
